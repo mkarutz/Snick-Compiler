d8359e034579e44f8a5ba7e92a86ff77b39bb45d
Malcolm Karutz <malcolm_karutz@live.com>
2017-05-02 15:43:38 +1000

    implemented test framework for brill code generation

--- run_tests.py
+++ run_tests.py
@@ -2,21 +2,68 @@ import glob
 import os
 import subprocess
 
+PPRINTER_TEST_DIR = "tests/pprinter/"
+BRILL_TEST_DIR = "tests/brill/"
+
+TEMP_BRILL_FILE = "run_tests.brill.tmp"
 TEMP_TEST_FILE = "run_tests.out.tmp"
 TEMP_DIFF_FILE = "run_tests.diff.tmp"
 
-def get_tests():
+def get_brill_tests():
     tests = []
-    for file in glob.glob("tests/*.snick"):
-        tests.append((file, file + ".out"))
+    for fn in glob.glob(BRILL_TEST_DIR + "*.snick"):
+        tests.append((fn, fn[:-6] + ".in", fn[:-6] + ".out"))
     return tests
 
-def run_test(test, expected):
+def get_pprinter_tests():
+    tests = []
+    for fn in glob.glob(PPRINTER_TEST_DIR + "*.snick"):
+        tests.append((fn, fn + ".out"))
+    return tests
+    
+def run_brill_test(test):
+    source_fn, input_fn, expected_fn = test
+    
+    # generate brill code
+    with open(TEMP_BRILL_FILE, 'w') as out_file:
+        subprocess.call(['./snick', source_fn], stdout=out_file)
+    
+    # run brill code    
+    with open(TEMP_TEST_FILE, 'w') as out_file:
+        if not os.path.isfile(input_fn):
+            subprocess.call([
+                './brill', 
+                TEMP_BRILL_FILE
+            ], stdout=out_file)
+        else:
+            with open(TEMP_BRILL_FILE, 'r') as brill_file:
+                subprocess.call([
+                    './brill', 
+                    TEMP_BRILL_FILE
+                ], stdin=brill_file, stdout=out_file)
+    
+    # diff program output
+    with open(TEMP_DIFF_FILE, 'w') as diff_file:
+        subprocess.call([
+            'diff', 
+            '--ignore-trailing-space', 
+            '--strip-trailing-cr', 
+            expected_fn, 
+            TEMP_TEST_FILE
+        ], stdout=diff_file)
+            
+    with open(TEMP_DIFF_FILE, 'r') as diff_file:
+        diff = diff_file.read()
+        return diff
+
+def run_pprinter_test(test):
+    source_fn, expected_fn = test
+    
     with open(TEMP_TEST_FILE, 'w') as out_file:
         subprocess.call([
             './snick', 
             '-p', 
-            test
+            source_fn
         ], stdout=out_file, stderr=out_file)
                         
     with open(TEMP_DIFF_FILE, 'w') as diff_file:
@@ -24,7 +71,7 @@ def run_test(test, expected):
             'diff', 
             '--ignore-trailing-space', 
             '--strip-trailing-cr', 
-            expected, 
+            expected_fn, 
             TEMP_TEST_FILE
         ], stdout=diff_file)
             
@@ -37,9 +84,23 @@ def run_tests():
     print("===================================================================")
     num_failures = 0
     num_success = 0
-    for test, expected in get_tests():
-        print("Running test: %s" % test)
-        diff = run_test(test, expected)
+    for test in get_pprinter_tests():
+        print("Running test: %s" % test[0])
+        diff = run_pprinter_test(test)
+        cleanup()
+        if (diff != ""):
+            num_failures += 1
+            print("FAILED: Output did not match expectation.")
+            print(diff)
+        else:
+            print("PASSED")
+            num_success += 1
+        print("")
+    
+    for test in get_brill_tests():
+        print("Running test: %s" % test[0])
+        diff = run_brill_test(test)
+        cleanup()
         if (diff != ""):
             num_failures += 1
             print("FAILED: Output did not match expectation.")
@@ -53,10 +114,14 @@ def run_tests():
     print("Tests finished: %d FAILED / %d PASSED"  
             % (num_failures, num_success))
 
+def remove_ifexists(fn):
+    if os.path.isfile(fn):
+        os.remove(fn)
+
 def cleanup():
-    os.remove(TEMP_TEST_FILE)
-    os.remove(TEMP_DIFF_FILE)
+    remove_ifexists(TEMP_TEST_FILE)
+    remove_ifexists(TEMP_DIFF_FILE)
+    remove_ifexists(TEMP_BRILL_FILE)
 
 if __name__ == "__main__":
     run_tests()
-    cleanup()
